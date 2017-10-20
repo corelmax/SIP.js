5a0e32ec0b4771ef8e54b897ff90b5c1a4bc7db0
Anucha Petchagun <big@lensod.com>
2017-10-20 13:59:00 +0700

    Fix stream.stop is not defined

--- src/WebRTC/MediaStreamManager.js
+++ src/WebRTC/MediaStreamManager.js
@@ -132,7 +132,7 @@ MediaStreamManager.prototype = Object.create(SIP.EventEmitter.prototype, {
   'release': {value: function release (stream) {
     var streamId = MediaStreamManager.streamId(stream);
     if (this.acquisitions[streamId] === false) {
-      stream.stop();
+      stream.getTracks().forEach(function (track) { track.stop(); });
     }
     delete this.acquisitions[streamId];
   }},
